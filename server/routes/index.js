e517f42695d7554e4c13bae98e695708caa6dcae
unknown <you@example.com>
2020-08-15 19:39:10 +0900

    db connect

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var sqlite3= require('sqlite3').verbose();
+var db= new sqlite3.Database('data.db');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send("hello");
+  res.render('index');
 });
 
 module.exports = router;
