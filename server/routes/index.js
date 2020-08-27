e47399806e3214856ce04a6657b1b30ab55c6d97
unknown <you@example.com>
2020-08-27 15:28:40 +0900

    db에 data넣음 /에서 get 에러

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,8 +4,16 @@ var sqlite3= require('sqlite3').verbose();
 var db= new sqlite3.Database('data.db');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index');
-});
+
+router.get("/", function (req, res, next) {
+  const query = `select * from board `;
+  db.serialize(function () {
+    db.all("query", function (err, rows) {
+      if (err) res.status(500).send("db")
+      else {console.log(rows[0])
+        res.render("all", { datas: rows })}
+    })
+  })
+})
 
 module.exports = router;
